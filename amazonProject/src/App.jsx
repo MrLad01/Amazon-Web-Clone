35b84adb9f466523750ea09fd2352b0e14680a25
MrLad01 <ladayodeji@gmail.com>
2023-06-20 11:11:23 +0100

    update

--- amazonProject/src/App.jsx
+++ amazonProject/src/App.jsx
@@ -1,12 +1,21 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 
 
 function App() {
  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        
+      </Route>
+    )
+  )
 
   return (
     <>
+      <RouterProvider router={router} />
     </>
   )
 }
