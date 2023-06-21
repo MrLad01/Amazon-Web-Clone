072253cf6b16ccdda0a8a3bd9e3c32c23e088659
MrLad01 <ladayodeji@gmail.com>
2023-06-22 00:07:50 +0100

    update

--- amazonProject/src/components/Body.jsx
+++ amazonProject/src/components/Body.jsx
@@ -1,9 +1,30 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Autoplay } from 'swiper';
+
+// Import Swiper styles
+// import 'swiper/css/pagination';
+// import 'swiper/css/scrollbar';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 
 const Body = () => {
   return (
-    <div>
-      
-    </div>
+    <Swiper
+        modules={[Navigation, Autoplay]}
+        navigation
+        autoplay
+        slidesPerView={1}
+    // install Swiper modules
+    // pagination={{ clickable: true }}
+    // scrollbar={{ draggable: true }}
+  >
+    <SwiperSlide>Slide 1</SwiperSlide>
+    <SwiperSlide>Slide 2</SwiperSlide>
+    <SwiperSlide>Slide 3</SwiperSlide>
+    <SwiperSlide>Slide 4</SwiperSlide>
+    ...
+  </Swiper>
   )
 }
 
