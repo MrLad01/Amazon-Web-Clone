f761a7abd448c56687fa5ef8b367b5bda48ccbaf
MrLad01 <ladayodeji@gmail.com>
2023-06-24 22:34:36 +0100

    update

--- amazonProject/src/data/data2.js
+++ amazonProject/src/data/data2.js
@@ -1,17 +1,111 @@
+import img19 from "../images/img19.jpg"
+import img20 from "../images/img20.jpg"
+import img21 from "../images/img21.jpg"
+import img22 from "../images/img22.jpg"
+import img23 from "../images/img23.jpg"
+import img24 from "../images/img24.jpg"
+import img25 from "../images/img25.jpg"
+import img27 from "../images/img27.jpg"
+import img28 from "../images/img28.jpg"
+import img29 from "../images/img29.jpg"
+import img30 from "../images/img30.jpg"
+import img31 from "../images/img31.jpg"
+import img32 from "../images/img32.jpg"
+import img33 from "../images/img33.jpg"
+import img34 from "../images/img34.jpg"
+import img35 from "../images/img35.jpg"
+import img36 from "../images/img36.jpg"
+import img37 from "../images/img37.jpg"
+import img38 from "../images/img38.jpg"
+import img39 from "../images/img39.jpg"
+import img40 from "../images/img40.jpg"
+import img41 from "../images/img41.jpg"
+import img42 from "../images/img42.jpg"
+import img43 from "../images/img43.jpg"
+import img44 from "../images/img44.jpg"
+import img45 from "../images/img45.jpg"
+import img46 from "../images/img46.jpg"
+import img47 from "../images/img47.jpg"
+import img48 from "../images/img48.jpg"
+import img49 from "../images/img49.jpg"
+import img50 from "../images/img50.jpg"
+import img51 from "../images/img51.jpg"
+import img52 from "../images/img52.jpg"
+import img53 from "../images/img53.jpg"
+import img54 from "../images/img54.jpg"
+import img55 from "../images/img55.jpg"
+import img56 from "../images/img56.jpg"
+import img57 from "../images/img57.jpg"
+import img58 from "../images/img58.jpg"
+import img59 from "../images/img50.jpg"
+import img60 from "../images/img60.jpg"
+import img61 from "../images/img61.jpg"
+import img62 from "../images/img62.jpg"
+import img63 from "../images/img63.jpg"
+import img64 from "../images/img64.jpg"
+
+
 const data2 = [
     [
         {
             id: 0,
             head: "Trending deals",
             link: "See all deals",
-            img: []
+            img: [
+                img49,
+                img50,
+                img51,
+                img52,
+                img53,
+                img54,
+                img55,
+                img56,
+                img57,
+                img58,
+                img59,
+                img60,
+                img61,
+                img62,
+                img63,
+                img64
+            ]
         }
     ],
     [
         {
             id: 1,
             head: "Best Sellers in Home & Kitchen",
-            img: []
+            img: [
+                img19,
+                img20,
+                img21,
+                img22,
+                img23,
+                img24,
+                img25,
+                img27,
+                img28,
+                img29,
+                img30,
+                img31,
+                img32,
+                img33,
+                img34,
+                img35,
+                img36,
+                img37,
+                img38,
+                img39,
+                img40,
+                img41,
+                img42,
+                img43,
+                img44,
+                img45,
+                img46,
+                img47,
+                img48
+            ]
         }
     ]
 ]
--- amazonProject/src/subcomponents/Slide.jsx
+++ amazonProject/src/subcomponents/Slide.jsx
@@ -1,16 +1,41 @@
 import { Link } from "react-router-dom"
 import data2 from "../data/data2"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Autoplay, Scrollbar } from 'swiper';
 
-const Slider = ({number, deal}) => {
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/scrollbar';
+
+const Slider = ({number, deal, discount}) => {
 
   const files = data2[number].map((file) => {
   return (
-    <div className="bg-white h-72 w-324 px-5 py-6">
+    <div className="bg-white h-72 w-324 px-5 py-6 grid">
         <div className='flex gap-x-3'>
             <h2 className='text-xl font-bold'>{file.head}</h2>
            {deal && <Link to="" className='text-sm text-blue-500 mt-1'>{file.link}</Link>}
         </div>
-    </div> 
+        <div>
+          <Swiper
+              modules={[Navigation, Autoplay, Scrollbar]}
+              navigation
+              autoplay
+              slidesPerView={3}
+  >
+            { file.img.map((image) => (
+              
+              <SwiperSlide>
+              <img src={image} alt="" />
+              </SwiperSlide>
+              ))
+    
+              }
+          </Swiper>
+
+        </div>
+        <div>Hello World</div>
+    </div>
      )
   })
 
