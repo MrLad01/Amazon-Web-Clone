0527e12fa46da4b390072c5779a5747c53b4d312
MrLad01 <ladayodeji@gmail.com>
2023-06-27 02:27:28 +0100

    update

--- amazonProject/src/data/data3.js
+++ amazonProject/src/data/data3.js
@@ -1,5 +1,55 @@
-const data3 = [
+import img100 from "../images/img100.jpg"
+import img102 from "../images/img102.jpg"
+import img103 from "../images/img103.jpg"
+import img104 from "../images/img104.jpg"
+import img105 from "../images/img105.jpg"
 
+const data3 = [
+    [
+        {
+            id:1,
+            img: img100,
+            cap: "Bodhi Dog Natural Dog Calming Spray | Premium Lavender Scented Deodorizing Spray for Dogs and Cats | Supports Relief For Travel, Vet Visits and Kennel Stays | Made in USA (Calming Lavender, 8 Fl Oz)",
+            price1: 17,
+            price2: 99
+        }
+    ],
+    [
+        {
+            id:2,
+            img: img103,
+            cap: "Bodhi Dog Sunscreen | Large 8oz SPF 30+ Moisturizing Pet Sunscreen | Safe for All Breeds of Dogs, Cats and Horses | Natural Skin Protection and Conditioner for Skin, Coat, Nose, and Ears | USA Made",
+            price1: 13,
+            price2: 99
+        }
+    ],
+    [
+        {
+            id:3,
+            img: img104,
+            cap: "Organic Paw Balm for Dogs & Cats | All Natural Soothing & Healing for Dry Cracking Rough Pet Skin | Protect & Restore Cracked and Chapped Dog Paws & Pads | Better Than Paw Wax (Paw Balm, 2 oz)",
+            price1: 14,
+            price2: 99
+        }
+    ],
+    [
+        {
+            id:4,
+            img: img102,
+            cap: "USDA Certified Organic Nose Balm for Dogs & Cats | Natural Soothing & Healing for Dry Cracking Rough Pet Skin | Protect & Restore Cracked and Chapped Dog Noses (Nose Balm, 2 oz)",
+            price1: 14,
+            price2: 99
+        }
+    ],
+    [
+        {
+            id:5,
+            img: img105,
+            cap: "Pet Grooming Wipes | Conditions and Deodorizes Coat | No Parabens or SLS | Large Wet & Thick Cleaning Best for Dogs and Cats (Mango, 75CT)",
+            price1: 13,
+            price2: 99
+        }
+    ],
 ]
 
 export default data3;
--- amazonProject/src/subcomponents/Card2.jsx
+++ amazonProject/src/subcomponents/Card2.jsx
@@ -2,16 +2,20 @@ import { Swiper } from "swiper/react"
 import data3 from "../data/data3"
 
 
-const Card2 = ({topic,slide, discount, ph, pw, ih, iw}) => {
+const Card2 = ({topic,slide, discount, ph, pw, ih, iw, c, more, number}) => {
+
+    data3[{number}].map((data)=>{ 
+
   return (
     <div className= {`h-${ph} w-${pw} grid`}>
-      {topic && <h2>{}</h2>}
+      {topic && <h2>{data.title}</h2>}
       {slide ? Swiper :
-      <img src="" alt="" className={`h-${ih} w-${iw}`} />}
-      <h2></h2>
+      <img src={data.img} alt="" className={`h-${ih} w-${iw}`} />}
+      <h2 className={`line-clamp-${c}`}>{data.cap}</h2>
       <div className="flex">
-        <span></span>
-        <span></span>
+        <span>$</span>
+        <span>{data.price1}</span>
+        <span>{data.price2}</span>
         {discount && <span></span>}
         {more && 
         <div className="flex">
@@ -23,7 +27,7 @@ const Card2 = ({topic,slide, discount, ph, pw, ih, iw}) => {
         }
       </div>
     </div>
-  )
+  ) })
 }
 
 export default Card2
