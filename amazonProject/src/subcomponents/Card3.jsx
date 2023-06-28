aa0d029c8bd1d3905d2c7261ad28dd01d40a316c
MrLad01 <ladayodeji@gmail.com>
2023-06-28 20:31:09 +0100

    update

--- amazonProject/src/subcomponents/Card3.jsx
+++ amazonProject/src/subcomponents/Card3.jsx
@@ -14,7 +14,7 @@ const Card3 = ({discount, ph, pw, ih, iw, more, number, s, tw}) => {
     const [swap4, setSwap4] = useState(data.discount[0])
 
   return (
-    <div className= {`h-${ph} w-${pw} grid bg-white px-5 content-around `}>
+    <div className= {`h-${ph} w-${pw} grid bg-white px-5 py-6 content-around `}>
       <h2 className='text-xl font-bold' >{data.title}</h2>
       <img src={swap} alt="" className={`h-${ih} w-${iw} mb-3 object-scale-down`} />
       <div className={` flex flex-wrap my-2 `}>
@@ -35,28 +35,28 @@ const Card3 = ({discount, ph, pw, ih, iw, more, number, s, tw}) => {
                 setSwap2(data.price1[0])
                 setSwap3(data.price2[0])
                 setSwap4(data.discount[0])
-                }} className="h-16 w-15" />
+                }} className="h-16 w-15 rounded-md border p-1 border-black" />
             <img src={data.more[1]} alt="" onClick={() => {
                 setSwap(data.more[1])
                 setSwap1(data.cap[1])
                 setSwap2(data.price1[1])
                 setSwap3(data.price2[1])
                 setSwap4(data.discount[1])
-                }} className="h-16 w-15" />
+                }} className="h-16 w-15 rounded-md border p-1 border-black" />
             <img src={data.more[2]} alt="" onClick={() => {
                 setSwap(data.more[2])
                 setSwap1(data.cap[2])
                 setSwap2(data.price1[2])
                 setSwap3(data.price2[2])
                 setSwap4(data.discount[2])
-                }} className="h-16 w-15" />
+                }} className="h-16 w-15 rounded-md border p-1 border-black" />
             <img src={data.more[3]} alt="" onClick={() => {
                 setSwap(data.more[3])
                 setSwap1(data.cap[3])
                 setSwap2(data.price1[3])
                 setSwap3(data.price2[3])
                 setSwap4(data.discount[3])
-                }} className="h-16 w-15" />
+                }} className="h-16 w-15 rounded-md border p-1 border-black" />
         </div>
         }
       </div>
