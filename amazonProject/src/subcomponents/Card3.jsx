fd607f3052a4ba13ad55cff05918c0103b66b824
MrLad01 <ladayodeji@gmail.com>
2023-06-28 20:41:44 +0100

    update

--- amazonProject/src/subcomponents/Card3.jsx
+++ amazonProject/src/subcomponents/Card3.jsx
@@ -14,19 +14,19 @@ const Card3 = ({discount, ph, pw, ih, iw, more, number, s, tw}) => {
     const [swap4, setSwap4] = useState(data.discount[0])
 
   return (
-    <div className= {`h-${ph} w-${pw} grid bg-white px-5 py-6 content-around `}>
+    <div className= {`h-${ph} w-${pw} grid bg-white px-5 pt-2 pb-6 content-around `}>
       <h2 className='text-xl font-bold' >{data.title}</h2>
-      <img src={swap} alt="" className={`h-${ih} w-${iw} mb-3 object-scale-down`} />
+      <img src={swap} alt="" className={`h-${ih} w-${iw} mb-3 object-scale-down justify-self-center`} />
       <div className={` flex flex-wrap my-2 `}>
-        <h6 className={`line-clamp-2 text-${s} w-${tw}`}>{swap1}</h6>
+        <h6 className={`-mt-8 line-clamp-2 text-${s} w-${tw}`}>{swap1}</h6>
       </div>
      <div className="flex">
         <span className="text-xxs">$</span>
         <span className="text-lg -mt-1">{swap2}</span>
         <span className="text-xxs">{swap3}{swap3}</span>
-        {discount && <div className="text-sm">List: <span >{swap4}</span></div>} 
+        {discount && <div className="text-sm ml-2">List: <span >{swap4}</span></div>} 
      </div>
-     <div className="flex">
+     <div className="flex justify-self-center">
         {more && 
         <div className="flex gap-x-2">
             <img src={data.more[0]} alt="" onClick={() => {
@@ -35,28 +35,28 @@ const Card3 = ({discount, ph, pw, ih, iw, more, number, s, tw}) => {
                 setSwap2(data.price1[0])
                 setSwap3(data.price2[0])
                 setSwap4(data.discount[0])
-                }} className="h-16 w-15 rounded-md border p-1 border-black" />
+                }} className="h-16 w-16 object-contain rounded-md border p-1 border-black" />
             <img src={data.more[1]} alt="" onClick={() => {
                 setSwap(data.more[1])
                 setSwap1(data.cap[1])
                 setSwap2(data.price1[1])
                 setSwap3(data.price2[1])
                 setSwap4(data.discount[1])
-                }} className="h-16 w-15 rounded-md border p-1 border-black" />
+                }} className="h-16 w-16 object-contain rounded-md border p-1 border-black" />
             <img src={data.more[2]} alt="" onClick={() => {
                 setSwap(data.more[2])
                 setSwap1(data.cap[2])
                 setSwap2(data.price1[2])
                 setSwap3(data.price2[2])
                 setSwap4(data.discount[2])
-                }} className="h-16 w-15 rounded-md border p-1 border-black" />
+                }} className="h-16 w-16 object-contain rounded-md border p-1 border-black" />
             <img src={data.more[3]} alt="" onClick={() => {
                 setSwap(data.more[3])
                 setSwap1(data.cap[3])
                 setSwap2(data.price1[3])
                 setSwap3(data.price2[3])
                 setSwap4(data.discount[3])
-                }} className="h-16 w-15 rounded-md border p-1 border-black" />
+                }} className="h-16 w-16 object-contain rounded-md border p-1 border-black" />
         </div>
         }
       </div>
